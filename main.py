c9aa14314c8c6f3f8e6e92a0e510608df278fbf3
Mohammadamin Rami <ramimohammadamin@gmail.com>
2025-02-22 17:01:36 +0330

    add: functionalities of the main file

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+from dotenv import load_dotenv
+from utils import create_run_inputs, scrape
+import os
+from apify_client import ApifyClientAsync
+import json
+import asyncio
+
+
+load_dotenv()
+
+
+API_TOKEN = os.getenv("APIFY_API_TOKEN")
+ACTOR_ID = os.getenv("ACTOR_ID")
+MIN_PRICE = os.getenv("MIN_PRICE")
+MAX_PRICE = os.getenv("MAX_PRICE")
+runners_per_room = json.loads(os.getenv("RUNNERS_PER_ROOM"))
+
+
+client = ApifyClientAsync(API_TOKEN)
 template = {
         "adults": None,
         "rooms": None,
         "minMaxPrice": None,
-        "checkIn": None,
-        "checkOut": None,
+        "checkIn": os.getenv("CHECK_IN"),
+        "checkOut": os.getenv("CHECK_OUT"),
         "currency": "USD",
         "language": "en-gb",
         "maxItems": 100000,
@@ -11,3 +30,5 @@ template = {
         "search": "Dubai",
         "starsCountFilter": "any"
     }
+run_inputs = create_run_inputs(runners_per_room, (MIN_PRICE, MAX_PRICE), template)
+asyncio.run(scrape(client, ACTOR_ID, run_inputs))
